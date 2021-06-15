7d615f7cf0b6b0bce42e687abf74e79c63eac740
Vipul Mistry <vipulmistry92@gmail.com>
2021-06-15 14:29:58 +0530

    vipul-add mysql database

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cors         = require('cors');
 const bodyParser   = require('body-parser');
 const jwt          = require('./helpers/jwt')
 const errorHandler = require('./helpers/error-handler');
-
+const mqtt         = require('./mqtt_data');
+const mqtt1         = require('./mqtt_mysql');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
---
+++ mqtt_data/mqtt_data.service.js
@@ -0,0 +1,41 @@
+const config = require('../config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('../helpers/db');
+const Mqtt_data = db.Mqtt_data;
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    delete: _delete
+};
+
+async function getAll() {
+    return await Mqtt_data.find();
+}
+
+async function getById(id) {
+    return await Mqtt_data.findById(id);
+}
+async function create(dataParam) {
+    // // validate
+    // if (await User.findOne({ username: userParam.username })) {
+    //     throw 'Username "' + userParam.username + '" is already taken';
+    // }
+
+    const mqtt_data = new Mqtt_data(dataParam);
+
+    // // hash password
+    // if (userParam.password) {
+    //     user.hash = bcrypt.hashSync(userParam.password, 10);
+    // }
+
+    // save user
+    await mqtt_data.save();
+}
+
+
+async function _delete(id) {
+    await Mqtt_data.findByIdAndRemove(id);
+}
