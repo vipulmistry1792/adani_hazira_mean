bac512fba4e9bc96c8cf542f823a253624bc167c
Vipul Mistry <vipulmistry92@gmail.com>
2021-06-16 10:38:09 +0530

    vipul-c

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const cors         = require('cors');
 const bodyParser   = require('body-parser');
 const jwt          = require('./helpers/jwt')
 const errorHandler = require('./helpers/error-handler');
-const mqtt         = require('./mqtt_data');
-const mqtt1         = require('./mqtt_mysql');
+//const mqtt         = require('./mqtt_data');
+const path         = require('path');
+//const mqtt1         = require('./mqtt_mysql');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 // use JWT auth to secure the api
-app.use(jwt());
-
+//app.use(jwt());
+//set static folder 
+app.use(express.static(path.join(__dirname, 'static')))
 // api routes
 app.use('/users', require('./users/user.controller'));
 
 // global error handler
-app.use(errorHandler);
+//app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
